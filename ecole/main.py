0155c55500ae043d51f67a039e68b6a31bf8eb89
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-10 15:50:04 +0200

    Test des readall plus méthode d'affichage de school

--- ecole/main.py
+++ ecole/main.py
@@ -6,16 +6,6 @@ Application de gestion d'une école
 """
 
 from business.school import School
-from datetime import date
-
-from daos.dao import Dao
-from daos.teacher_dao import TeacherDao
-from daos.course_dao import CourseDao
-from daos.student_dao import StudentDao
-from models.teacher import Teacher
-from models.course import Course
-from models.student import Student
-from models.address import Address
 
 
 def main() -> None:
@@ -27,29 +17,38 @@ Bienvenue dans notre école
 
     school: School = School()
 
+    # Test readall teacher
+    #teacher_dao = TeacherDao()
+    #teachers = teacher_dao.read_all()
+    #for teacher in teachers:
+        #print(teacher)
+
+    # Test readall course
+    #course_dao = CourseDao()
+    #courses = course_dao.read_all()
+    #for course in courses:
+        #print(course)
+
+    #Test readall student
+    #student_dao = StudentDao()
+    #students = student_dao.read_all()
+    #for student in students:
+        #print(student)
+        #for course in student.courses_taken:
+            #print(f" - {course}")
+        #print()
+
+
     # initialisation d'un ensemble de cours, enseignants et élèves composant l'école
-    school.init_static()
-
-    #Test
-    student_dao = StudentDao()
-    student = student_dao.read(9)
-    print("Etudiant récupéré")
-    print(student)
-
-
-    result = student_dao.delete(student)
-    print("Suppression :", result)
-    student_chek = student_dao.read(9)
-    print("Après suppression :", student_chek)
-
+    school.load_from_database()
 
     # affichage de la liste des cours, leur enseignant et leurs élèves
     print()
     school.display_courses_list()
 
-    print(school.get_course_by_id(1))
-    print(school.get_course_by_id(2))
-    print(school.get_course_by_id(9))
+    #print(school.get_course_by_id(1))
+    #print(school.get_course_by_id(2))
+    #print(school.get_course_by_id(9))
 
 
 if __name__ == '__main__':
