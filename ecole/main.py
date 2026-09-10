fc2522b42edc08bff304d2ebb839c1beb4222f3c
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-09-10 08:54:11 +0200

    Ajout des Test CRUD pour student

--- ecole/main.py
+++ ecole/main.py
@@ -8,10 +8,14 @@ Application de gestion d'une école
 from business.school import School
 from datetime import date
 
+from daos.dao import Dao
 from daos.teacher_dao import TeacherDao
 from daos.course_dao import CourseDao
+from daos.student_dao import StudentDao
 from models.teacher import Teacher
 from models.course import Course
+from models.student import Student
+from models.address import Address
 
 
 def main() -> None:
@@ -26,37 +30,17 @@ Bienvenue dans notre école
     # initialisation d'un ensemble de cours, enseignants et élèves composant l'école
     school.init_static()
 
-    """Test
-    teacher = Teacher(
-        "Victor", "Hugo", 23, date(2023, 9, 4)
-    )
-    teacher.id =1
+    #Test
+    student_dao = StudentDao()
+    student = student_dao.read(9)
+    print("Etudiant récupéré")
+    print(student)
 
-    course = Course("Python", date(2024, 3, 18), date(2024, 3, 29))
-    course.set_teacher(teacher)
-    course_dao = CourseDao()
-    id_course = course_dao.create(course)
-    print(f"Course créé avec l'id : {id_course}")
-    print(course)
 
-    course_read = course_dao.read(id_course)
-    print("Course lu depuis la BDD :")
-    print(course_read)
-
-    course_not_found = course_dao.read(9999)
-    print(f"Course inexistant : {course_not_found}")
-
-    course.name = "Python avancé"
-    result = course_dao.update(course)
-    print(f"Résultat de la mise à jour : {result}")
-    course_update = course_dao.read(course.id)
-    print("Course après mise à jour :")
-    print(course_update)
-
-    result = course_dao.delete(course)
-    print(f"Résultat de la suppression : {result}")
-    course_deleted = course_dao.read(course.id)
-    print(f"Course après suppression : {course_deleted}")"""
+    result = student_dao.delete(student)
+    print("Suppression :", result)
+    student_chek = student_dao.read(9)
+    print("Après suppression :", student_chek)
 
 
     # affichage de la liste des cours, leur enseignant et leurs élèves
